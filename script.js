ea3bfe9000cbbef5dea3e7b6fe7f9c2597b901d1
artsnow12 <168270648+artsnow12@users.noreply.github.com>
2025-01-07 13:40:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const stringType = "Hello, World!";  
+const numberType = 42;              
+const bigintType = 12345678901234567890n;  
+const booleanType = true;           
+const undefinedType = undefined;    
+
+
+
+console.log("stringType:", typeof stringType);    
+console.log("numberType:", typeof numberType);    
+console.log("bigintType:", typeof bigintType);    
+console.log("booleanType:", typeof booleanType);  
+console.log("undefinedType:", typeof undefinedType); 
+
