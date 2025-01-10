77934c462dfe14764b0ea312304f1abe17d6a2fb
artsnow12 <168270648+artsnow12@users.noreply.github.com>
2025-01-11 00:58:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const stringType = "Hello, World!";
 const numberType = 42;              
 const bigintType = 12345678901234567890n;  
 const booleanType = true;           
-const undefinedType = undefined;    
+const undefinedType = undefined;
+const nullType = null;              
+const objectType = { key: "value" }; 
+const arrayType = [1, 2, 3];        
+const functionType = function()   
 
 
 
@@ -12,4 +16,9 @@ console.log("numberType:", typeof numberType);
 console.log("bigintType:", typeof bigintType);    
 console.log("booleanType:", typeof booleanType);  
 console.log("undefinedType:", typeof undefinedType); 
+console.log("nullType:", typeof nullType);        
+console.log("objectType:", typeof objectType);    
+console.log("arrayType:", typeof arrayType);      
+console.log("functionType:", typeof functionType);
+console.log("symbolType:", typeof symbolType);
 
