3e002f455aa4fc3db1900002dd02726ad4cda0eb
artsnow12 <168270648+artsnow12@users.noreply.github.com>
2025-02-03 19:20:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-
-const stringType = "Hello, World!";  
-const numberType = 42;              
-const bigintType = 12345678901234567890n;  
-const booleanType = true;           
-const undefinedType = undefined;
-const nullType = null;              
-const objectType = { key: "value" }; 
-const arrayType = [1, 2, 3];        
-const functionType = function()   
-
-
-
-console.log("stringType:", typeof stringType);    
-console.log("numberType:", typeof numberType);    
-console.log("bigintType:", typeof bigintType);    
-console.log("booleanType:", typeof booleanType);  
-console.log("undefinedType:", typeof undefinedType); 
-console.log("nullType:", typeof nullType);        
-console.log("objectType:", typeof objectType);    
-console.log("arrayType:", typeof arrayType);      
-console.log("functionType:", typeof functionType);
-console.log("symbolType:", typeof symbolType);
-
+let company = { 
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 1600 }],
+    development: {
+      sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+      internals: [{name: 'Jack', salary: 1300}]
+    }
+  };
+  
+  function sumSalaries(department) {
+    if (Array.isArray(department)) { 
+      return department.reduce((prev, current) => prev + current.salary, 0); 
+    } else { 
+      let sum = 0;
+      for (let subdep of Object.values(department)) {
+        sum += sumSalaries(subdep); 
+      }
+      return sum;
+    }
+  }
+  
+  console.log('сума зарплат:', totalSalary);
+  
